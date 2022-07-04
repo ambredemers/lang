5e2522d959357dec42bd8dc080985a783305a5de
ambredemers <ambre.demers@gmail.com>
2022-07-04 18:48:02 +0200

    replaced std and, or, not, and neq with parsed strings instead of ocaml parse tree

--- std.ml
+++ std.ml
@@ -1,98 +1,20 @@
-let std : (string * Sexp_t.sexp_t) list =  [
-    ("true", Atom (Id "true"));
-    ("false", Atom (Id "false"));
-    ("nil", Atom (Id "nil"));
-    ("let", Error "attempted to use let as a variable");
-    ("lambda", Error "attempted to use lambda as a variable");
-    ("cons", Error "attempted to use cons as a variable");
-    ("car", Error "attempted to use car as a variable");
-    ("cdr", Error "attempted to use cdr as a variable");
-    ("eq", Error "attempted to use eq as a variable");
-    ("atom", Error "attempted to use atom as a variable");
-    ("quote", Error "attempted to use quote as a variable");
-    ("cond", Error "attempted to use cond as a variable");
-    ("and",
-        Pair (Atom (Id "lambda"), Pair (
-            Pair (Atom (Id "andl"), Pair (Atom (Id "andr"), Atom (Id "nil"))),
-            Pair (Pair (Atom (Id "cond"), Pair (
-                Pair (
-                    Pair (Atom (Id "eq"), Pair (Atom (Id "andl"), Pair (Atom (Id "true"), Atom (Id "nil")))),
-                    Pair (Pair (Atom (Id "cond"), Pair (
-                        Pair (Pair (
-                            Atom (Id "eq"),
-                            Pair (Atom (Id "andr"), Pair (Atom (Id "true"), Atom (Id "nil")))
-                        ), Pair (Atom (Id "true"), Atom (Id "nil"))),
-                        Pair (Pair (Pair (
-                            Atom (Id "eq"),
-                            Pair (Atom (Id "andr"), Pair (Atom (Id "false"), Atom (Id "nil")))
-                        ), Pair (Atom (Id "false"), Atom (Id "nil"))), Atom (Id "nil"))
-                    )), Atom (Id "nil"))
-                ),
-                Pair (Pair (
-                    Pair (Atom (Id "eq"), Pair (Atom (Id "andl"), Pair (Atom (Id "false"), Atom (Id "nil")))),
-                    Pair (Atom (Id "false"), Atom (Id "nil"))
-                ), Atom (Id "nil"))
-            )), Atom (Id "nil"))
-        ))
-    );
-    ("or",
-        Pair (Atom (Id "lambda"), Pair (
-            Pair (Atom (Id "orl"), Pair (Atom (Id "orr"), Atom (Id "nil"))),
-            Pair (Pair (Atom (Id "cond"), Pair (
-                Pair (
-                    Pair (Atom (Id "eq"), Pair (Atom (Id "orl"), Pair (Atom (Id "true"), Atom (Id "nil")))),
-                    Pair (Atom (Id "true"), Atom (Id "nil"))
-                ),
-                Pair (Pair (
-                    Pair (Atom (Id "eq"), Pair (Atom (Id "orl"), Pair (Atom (Id "false"), Atom (Id "nil")))),
-                    Pair (Pair (
-                        Atom (Id "cond"),
-                        Pair (
-                            Pair (Pair (
-                                Atom (Id "eq"),
-                                Pair (Atom (Id "orr"), Pair (Atom (Id "true"), Atom (Id "nil")))
-                            ), Pair (Atom (Id "true"), Atom (Id "nil"))),
-                            Pair (Pair (Pair (
-                                Atom (Id "eq"),
-                                Pair (Atom (Id "orr"), Pair (Atom (Id "false"), Atom (Id "nil")))
-                            ), Pair (Atom (Id "false"), Atom (Id "nil"))), Atom (Id "nil"))
-                        )
-                    ), Atom (Id "nil"))
-                ), Atom (Id "nil"))
-            )), Atom (Id "nil"))
-        ))
-    );
-    ("not",
-        Pair (Atom (Id "lambda"), Pair (Pair (Atom (Id "notx"), Atom (Id "nil")), Pair (Pair (
-            Atom (Id "cond"),
-            Pair (
-                Pair (Pair (
-                    Atom (Id "eq"),
-                    Pair (Atom (Id "notx"), Pair (Atom (Id "true"), Atom (Id "nil")))
-                ), Pair (Atom (Id "false"), Atom (Id "nil"))),
-                Pair (Pair (
-                    Pair (
-                        Atom (Id "eq"),
-                        Pair (Atom (Id "notx"), Pair (Atom (Id "false"), Atom (Id "nil")))
-                    ),
-                    Pair (Atom (Id "true"), Atom (Id "nil"))
-                ), Atom (Id "nil"))
-            )
-        ), Atom (Id "nil"))))
-    );
-    ("neq",
-        Pair (Atom (Id "lambda"), Pair (
-            Pair (Atom (Id "neql"), Pair (Atom (Id "neqr"), Atom (Id "nil"))),
-            Pair (Pair (
-                Atom (Id "not"),
-                Pair (
-                    Pair (Atom (Id "eq"), Pair (
-                        Atom (Id "neql"),
-                        Pair (Atom (Id "neqr"), Atom (Id "nil"))
-                    )),
-                    Atom (Id "nil")
-                )
-            ), Atom (Id "nil"))
-        ))
-    );
-]
+let std : (string * Sexp_t.sexp_t) list =
+    let f (x : string) : Sexp_t.sexp_t = Parse.parse (Lex.lex x) in
+    [
+        ("true", Atom (Id "true"));
+        ("false", Atom (Id "false"));
+        ("nil", Atom (Id "nil"));
+        ("let", Error "attempted to use let as a variable");
+        ("lambda", Error "attempted to use lambda as a variable");
+        ("cons", Error "attempted to use cons as a variable");
+        ("car", Error "attempted to use car as a variable");
+        ("cdr", Error "attempted to use cdr as a variable");
+        ("eq", Error "attempted to use eq as a variable");
+        ("atom", Error "attempted to use atom as a variable");
+        ("quote", Error "attempted to use quote as a variable");
+        ("cond", Error "attempted to use cond as a variable");
+        ("and", f "(.\\ (andl andr) (cond ((eq andl false) false) ((eq andl true) (cond ((eq andr true) true) ((eq andr false) false)))))");
+        ("or", f "(.\\ (orl orr) (cond ((eq orl true) true) ((eq orl false) (cond ((eq orr true) true) ((eq orr false) false)))))");
+        ("not", f "(.\\ (notx) (cond ((eq notx true) false) ((eq notx false) true)))");
+        ("neq", f "(.\\ (neql neqr) (not (eq neql neqr)))");
+    ]
