6b6a46fe676c035dbae239b1b9628f875eba2c2a
ambredemers <ambre.demers@gmail.com>
2022-07-11 20:51:40 -0400

    updated repl to handle expressions across multiple lines and to use semicolon as the separator between s-expressions

--- main.ml
+++ main.ml
@@ -1,11 +1,23 @@
 (* repl *)
 
+exception Semicolon
+
 let () =
     try (
         let input = ref (Stdlib.read_line ()) in 
-        while !input <> "q" do
-            print_string (Sexp_t.pretty_string_of_sexp (Interpret.interpret (Parse.parse (Lex.lex !input))));
-            print_string "\n";
-            input := Stdlib.read_line ()
+        while !input <> "quit" do
+            try (while true do
+                let temp = String.split_on_char ';' !input in
+                if List.length temp <> 1 then
+                    (input := List.hd temp;
+                    raise Semicolon)
+                else if List.hd temp = "quit" then
+                    raise End_of_file
+                else
+                    input := !input ^ "\n" ^ Stdlib.read_line ()    
+            done) with Semicolon -> 
+                print_string (Sexp_t.pretty_string_of_sexp (Interpret.interpret (Parse.parse (Lex.lex !input))));
+                print_string "\n";
+                input := Stdlib.read_line ()
         done
     ) with End_of_file -> ()
