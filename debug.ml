8896897711ff5ddfa0158493998da26b030c225a
ambredemers <ambre.demers@gmail.com>
2022-07-16 20:16:56 -0400

    updated debug repl to have same changes as regular repl

--- debug.ml
+++ debug.ml
@@ -1,14 +1,26 @@
 (* debug repl *)
 
+exception Semicolon
+
 let () =
     try (
         let input = ref (Stdlib.read_line ()) in 
-        while !input <> "q" do
-            let lex = Lex.lex !input in
-            print_string (Token_t.string_of_token_list lex);
-            print_string "\n";
-            print_string (Sexp_t.string_of_sexp (Parse.parse lex));
-            print_string "\n";
-            input := Stdlib.read_line ()
+        while !input <> "quit" do
+            try (while true do
+                let temp = String.split_on_char ';' !input in
+                if List.length temp <> 1 then
+                    (input := List.hd temp;
+                    raise Semicolon)
+                else if List.hd temp = "quit" then
+                    raise End_of_file
+                else
+                    input := !input ^ "\n" ^ Stdlib.read_line ()    
+            done) with Semicolon -> 
+                let lex = Lex.lex !input in
+                print_string (Token_t.string_of_token_list lex);
+                print_string "\n";
+                print_string (Sexp_t.string_of_sexp (Parse.parse lex));
+                print_string "\n";
+                input := Stdlib.read_line ()
         done
     ) with End_of_file -> ()
